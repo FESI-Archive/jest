25d4c29e5520b08a4d2755561eeefd94bb2f20d4
hyebinpark <hoonywhitesocks@gmail.com>
2026-02-27 17:39:51 +0900

    feat: resolves, rejects 매처 사용 검증

--- fetchMock.test.js
+++ fetchMock.test.js
@@ -9,21 +9,14 @@ test('사용자가 데이터를 성공적으로 가져오는 경우 모킹', asy
     json: jest.fn().mockResolvedValue(mockUserData),
   });
 
-  // 2. Promise 방식으로 테스트
-  return fetchUserData(1).then((userData) => {
-    expect(userData).toEqual(mockUserData);
-  });
+  // 2. resolves 방식으로 테스트
+  return expect(fetchUserData(1)).resolves.toEqual(mockUserData);
 });
 
-test('네트워크 오류 발생 시 에러 처리 - Promise 방식', () => {
+test('네트워크 오류 발생 시 에러 처리 - rejects 방식', () => {
   // 1. fetch가 거부된 Promise를 반환하도록 모킹
   global.fetch = jest.fn().mockRejectedValue(new Error('Network Error'));
 
-  // 2. 최소 1개의 assertion이 필요
-  expect.assertions(1);
-
-  // 3. Promise 방식으로 에러 처리 테스트
-  return fetchUserData(1).catch((error) => {
-    expect(error.message).toBe('Network Error');
-  });
+  // 2. rejects 방식으로 에러 처리 테스트
+  return expect(fetchUserData(1)).rejects.toThrow('Network Error');
 });
